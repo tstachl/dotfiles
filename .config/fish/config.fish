d93755f0a79335d23a398be708e20ef80476d551
Thomas Stachl <thomas@stachl.me>
2022-07-05 16:55:02 +0000

    yadm pull if connected

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
-if status --is-interactive; and not test -e "$HOME/.ssh/id_thomas"
-  yadm decrypt
-end
-
 if not functions -q fundle
   eval (curl -sfL https://git.io/fundle-install)
 end
@@ -9,5 +5,16 @@ end
 fundle plugin "pure-fish/pure"
 
 fundle init
+
+if status --is-interactive
+  if not test -e "$HOME/.ssh/id_thomas"
+    yadm decrypt
+  end
+
+  # check if we have internet connection and pull yadm  
+  if ping -c1 -W1 1.1.1.1 >/dev/null 2>&1
+    yadm pull
+  end
+end
 
 
