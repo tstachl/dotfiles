459d902d29eec0b93ade02c7c451c192ef3f0299
Thomas Stachl <thomas@stachl.me>
2022-07-05 01:01:56 +0000

    adding fundle and pur

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,12 @@ if status --is-interactive; and not test -e "$HOME/.ssh/id_thomas"
   yadm decrypt
 end
 
-# plugins
-fisher install pure-fish/pure
+if not functions -q fundle
+  eval (curl -sfL https://git.io/fundle-install)
+end
+
+fundle plugin "pure-fish/pure"
+
+fundle init
+
 
